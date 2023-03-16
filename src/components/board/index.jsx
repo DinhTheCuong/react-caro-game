5357ceb61c42b4510c23e2950a52fc0e24c98ca2
DINH THE CUONG <106668267+DinhTheCuong@users.noreply.github.com>
2023-03-17 00:51:53 +0700

    react caro game

---
+++ src/components/board/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Square from '../square';
+
+const Board = ({squares, onClick}) => {
+  const renderSquares = (numbers) => {
+    return numbers.map((number) => (
+      <Square
+        value={squares[number]}
+        onClick={() => onClick(number)}
+      />
+    ));
+  };
+  return (
+    <div>
+      <div className='board-row'>
+        {renderSquares([0, 1, 2])}
+      </div>
+      <div className='board-row'>
+        {renderSquares([3, 4, 5])}
+      </div>
+      <div className='board-row'>
+        {renderSquares([6, 7, 8])}
+      </div>
+    </div>
+  );
+};
+
+const checkWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (
+      squares[a] &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c]
+    ) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+export default Board;
